cb08f0438506ae1f72460051fa0ca7b99dae56d9
Ziad Elsayyad <ziad.elsyad16@gmail.com>
2026-06-12 14:28:36 +0300

    feat: add filtering, sorting, and pagination support to getAllJobs controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,7 +4,7 @@ import { StatusCodes } from "http-status-codes";
 
 export const getAllJobs = async (req, res) => {
   // const isAdmin = req.user.role === "admin";
-  const { search } = req.query;
+  const { search, jobType, jobStatus, sort } = req.query;
 
   const queryObject = {
     createdBy: req.user.userId,
@@ -17,11 +17,42 @@ export const getAllJobs = async (req, res) => {
     ];
   }
 
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  if (jobStatus && jobStatus !== "all") {
+    queryObject.jobStatus = jobStatus;
+  }
+
+  const sortOptions = {
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "position",
+    "z-a": "-position",
+  };
+
+  const KeyValue = sortOptions[sort] || sortOptions.newest;
+
+  // PAGINATION
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   const jobs = await JobModel.find(
     queryObject,
     // isAdmin ? {} : { createdBy: req.user.userId },
-  );
-  res.status(StatusCodes.OK).json({ jobs });
+  )
+    .sort(KeyValue)
+    .skip(skip)
+    .limit(limit);
+
+  const totalJobs = await JobModel.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, currentPage: page, numOfPages, jobs });
 };
 
 export const createJob = async (req, res) => {
