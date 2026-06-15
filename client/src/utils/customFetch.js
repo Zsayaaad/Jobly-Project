75a45e831580a720cea8df4bd80ee6fdf3a885ce
Ziad Elsayyad <ziad.elsyad16@gmail.com>
2026-06-15 15:42:22 +0300

    feat: update customFetch base URL for production

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "/api/v1",
+  // baseURL: "/api/v1",
+  // This is the actual backend link that Vercel provided
+  baseURL: "https://jobify-backend-6elpz3pus-sayyad-projects.vercel.app/api/v1",
+  // Very important for the cookies and Auth to work
+  withCredentials: true,
 });
 
 export default customFetch;
